e6b461cc0710e705b8fb863ef0265ce59e808a33
IshidaKeisuke <datenokei@gmail.com>
2022-05-31 09:04:47 +0900

    プロフィールの基本実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-    resources :articles do
-  end
+  resources :articles
+  resources :profiles
+  get '/contacts', to: 'contacts#index'
 end
