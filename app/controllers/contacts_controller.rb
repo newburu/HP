2c135f830bd77e3a043594256c80ed07cc591b01
IshidaKeisuke <datenokei@gmail.com>
2022-06-05 14:49:15 +0000

    確認画面へ遷移まで終了

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,4 +2,15 @@ class ContactsController < ApplicationController
   def new
     @contact = Contact.new
   end
+
+  def confirm
+    @contact = Contact.new(contact_params)
+    render :new if @contact.invalid?
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :name_hiragana, :email, :matter, :menu, :discount, :payment, :content)
+  end
 end
