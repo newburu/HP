06c4fd5ab30fb633c236d3a816a4845e6ee7088e
IshidaKeisuke <datenokei@gmail.com>
2022-06-25 01:24:54 +0900

    確認画面まで実装

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,13 +5,18 @@ class ContactsController < ApplicationController
   
   def calendar
     @reservations = Reservation.all.where("day >= ?", Date.current).where("day < ?", Date.current >> 3).order(day: :desc)
+    @contact = Contact.new(contact_params)
   end
 
   def reservation
     @day = params[:day]
     @time = params[:time]
+    @reservation = Reservation.new(reservation_params)
+    @contact = Contact.new(contact_params)
   end
+  
   def confirm
+    @reservation = Reservation.new(reservation_params)
     @contact = Contact.new(contact_params)
     render :new if @contact.invalid?
   end
@@ -19,6 +24,10 @@ class ContactsController < ApplicationController
   private
 
   def contact_params
-    params.require(:contact).permit(:name, :name_hiragana, :email, :matter, :menu, :discount, :payment, :content)
+    params.permit(:name, :name_hiragana, :email, :matter, :menu, :discount, :payment, :content)
+  end
+
+  def reservation_params
+    params.permit(:day, :time)
   end
 end
