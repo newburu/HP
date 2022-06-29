23787b4bddaf47b1dfe76990337573cec7a8adf3
Ishida Keisuke <datenokei@gmail.com>
2022-06-29 14:48:44 +0000

    確認画面とバリデーション実装

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,26 +1,25 @@
 class ContactsController < ApplicationController
-  # before_action :set_contact_params, only: [:confirm]
-
   def new
     @contact = Contact.new
   end
   
   def calendar
+    @contact = Contact.new(contact_params)
+    render 'new' if @contact.invalid?
     @reservations = Reservation.all.where("day >= ?", Date.current).where("day < ?", Date.current >> 3).order(day: :desc)
     session[:name] = params[:contact][:name]
     session[:name_hiragana] = params[:contact][:name_hiragana]
     session[:email] = params[:contact][:email]
-    session[:matter] = params[:contact][:matter_text]
-    session[:menu] = params[:contact][:menu_text]
-    session[:discount] = params[:contact][:discount_text]
-    session[:payment] = params[:contact][:payment_text]
+    session[:matter] = params[:contact][:matter]
+    session[:menu] = params[:contact][:menu]
+    session[:discount] = params[:contact][:discount]
+    session[:payment] = params[:contact][:payment]
     session[:content] = params[:contact][:content]
   end
 
   def reservation
     @day = params[:day]
     @time = params[:time]
-    # render :new if @contact.invalid?
   end
   
   def confirm
@@ -33,10 +32,13 @@ class ContactsController < ApplicationController
     session[:discount]
     session[:payment]
     session[:content]
-    # render :new if @contact.invalid?
   end
 
   private
+  
+  def contact_params
+    params.require(:contact).permit(:name, :name_hiragana, :email, :matter, :menu, :discount, :payment, :content)
+  end
 
   def reservation_params
     params.permit(:day, :time, :start_time)
