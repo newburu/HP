0f2d60b82440a5582971abc2bb90dd7bc0a29384
IshidaKeisuke <datenokei@gmail.com>
2022-07-15 16:33:02 +0900

    active_storage追加

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -1,4 +1,25 @@
 class Admin::ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+  
   def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_parameter)
+    
+    #binding.pry
+    
+    if @article.save
+      redirect_to admin_articles_path
+    end
+  end
+
+  private
+
+  def article_parameter
+    params.require(:article).permit(:title, :body)
   end
 end
