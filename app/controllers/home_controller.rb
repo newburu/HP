5d10e3253f1d41a2da609685bc71e70b3ef6ef99
IshidaKeisuke <datenokei@gmail.com>
2022-07-27 08:42:06 +0900

    first-commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,2 @@
 class HomeController < ApplicationController
-  def top
-    if Home.present?
-      home_id = Home.order(updated_at: :desc).first.id
-      @home = Home.find(home_id)
-    end 
-  end
 end
