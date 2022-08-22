10853d43ecf545bc9208c019a9137ee6a89c7a0a
IshidaKeisuke <datenokei@gmail.com>
2022-08-22 22:01:13 +0900

    お問い合わせ一覧実装。ホーム画面も追記

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   devise_for :accounts, skip:[:registration, :password]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-
+  root to: 'homes#top'
   namespace :admin do
     resources :articles
+    resources :contacts,  except: [:show]
   end
   resources :profiles, :only => :index
   resource :contacts, :only => [:new, :create] do
