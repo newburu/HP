b0b983d56c944d6bba2cd3649402c8d117f65bc6
Ishidakeisuke <datenokei@gmail.com>
2022-09-02 07:18:35 +0000

    ページネーション実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,5 +2,10 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.where(status: "published").order(created_at: :desc)
     @articles = Kaminari.paginate_array(@articles).page(params[:page]).per(7)
+    @articles_per_month = Article.group("strftime('%Y年%m月', created_at)").count
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 end
