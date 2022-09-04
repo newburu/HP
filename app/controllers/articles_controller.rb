698fe93817ef33c2abeb823577fc1b7aace9d621
Ishidakeisuke <datenokei@gmail.com>
2022-09-04 02:22:56 +0000

    アーカイブの見た目作成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,14 @@
 class ArticlesController < ApplicationController
   def index
     @articles = Article.where(status: "published").order(created_at: :desc)
+    @articles_per_months = Article.order(created_at: :desc).group("strftime('%Y年%m月', created_at)").count
     @articles = Kaminari.paginate_array(@articles).page(params[:page]).per(7)
-    @articles_per_month = Article.group("strftime('%Y年%m月', created_at)").count
   end
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def archives
   end
 end
